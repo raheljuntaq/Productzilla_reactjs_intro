0aae5175966cff8323cc5fd1a3a2aa477b78a947
raheljuntaq <rahel.anastasyapithaloka2086@students.unila.ac.id>
2023-05-10 12:19:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class TodoList extends Component {
+  state = {
+    tasks: [
+      { id: 1, name: "Mengerjakan Post Test", completed: false },
+      { id: 2, name: "Mengerjakan Pre Test", completed: true },
+      { id: 3, name: "Menghadiri Bootcamp PX", completed: false },
+    ],
+  };
+
+  handleTaskClick = (id) => {
+    this.setState((prevState) => {
+      const updatedTasks = prevState.tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: !task.completed };
+        }
+        return task;
+      });
+      return { tasks: updatedTasks };
+    });
+  };
+
+  render() {
+    const tasks = this.state.tasks.map((task) => (
+      <li key={task.id} className={task.completed ? "completed" : ""}>
+        <input type="checkbox" checked={task.completed} onChange={() => this.handleTaskClick(task.id)} />
+        {task.name}
+      </li>
+    ));
+
+    return (
+      <div>
+        <ul>{tasks}</ul>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default TodoList;
